ff675f78397f8a3562cab42f537b963e0aab6209
Sherry <sherryc1@andrew.cmu.edu>
2015-10-07 12:10:32 -0400

    Added form for posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,49 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
+
+  def index
+  	@posts = Post.all
+  end
+
+  def show
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def edit
+  end
+
+  def create
+    @post = Post.new(post_params)
+
+    if @post.save
+      redirect_to posts_url, notice: 'Event was successfully created.'
+    else
+      render action: 'new'
+    end
+  end
+
+  def update
+    if @post.update(post_params)
+      redirect_to @post, notice: 'Congrats! You\'ve succesfully updated your event.'
+    else
+      render action: 'edit'
+    end
+  end
+
+  def destroy
+    @post.destroy
+    redirect_to posts_url
+  end
+
+  private
+    def set_post
+      @post = Post.find(params[:id])
+    end
+
+    def post_params
+      params.require(:post).permit(:owner_id, :title, :topic, :date, :start_time, :expected_duration, :details, :cancelled, :estimated_difficulty)
+    end
 end

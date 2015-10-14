467017c3adce64db203f35fb2b94a9687d59eee4
Sherry <sherryc1@andrew.cmu.edu>
2015-10-14 12:04:33 -0400

    Added my events section to index page.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,6 +3,7 @@ class PostsController < ApplicationController
 
   def index
   	@posts = Post.not_mine(current_user.id).chronological
+    @my_posts = Post.for_owner(current_user.id).chronological
   end
 
   def show

989be78efc59ae3fc2187b8ac947abf944f138d3
Hannah Stucken Douglas <h.s.douglas@gmail.com>
2015-10-19 16:19:39 -0400

    Sorting by workout type works. Separate page for search results in place.

--- config/routes.rb
+++ config/routes.rb
@@ -14,5 +14,7 @@ Rails.application.routes.draw do
   get 'login' => 'sessions#new', :as => :login
   get 'logout' => 'sessions#destroy', :as => :logout
 
+  get 'filter_workouts', :to => 'posts#filter_workouts'
+
   root to: 'home#index', as: :home
 end

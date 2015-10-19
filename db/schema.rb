4622cb12efd7b5bf93dabef3526fe017e54a0f7b
Hannah Stucken Douglas <h.s.douglas@gmail.com>
2015-10-19 15:22:32 -0400

    User can successfully add a photo and view it as part of their profile.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151009201037) do
+ActiveRecord::Schema.define(version: 20151019191457) do
 
   create_table "group_members", force: :cascade do |t|
     t.integer  "groupId"
@@ -76,6 +76,7 @@ ActiveRecord::Schema.define(version: 20151009201037) do
     t.boolean  "active"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.string   "photo"
   end
 
 end
